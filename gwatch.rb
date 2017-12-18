87099dcb43c2543d9822269d7636800a6165a28a
Robert Ely <robertely@gmail.com>
2017-12-17 22:31:44 -0700

    version 0.0.2 corrected darwin release

--- gwatch.rb
+++ gwatch.rb
@@ -1,8 +1,8 @@
 class Gwatch < Formula
   desc "graphing watch: expects numerical values, graphs the first one it sees."
   homepage "https://github.com/robertely/gwatch"
-  url "https://bintray.com/robertely/homebrew-gwatch/download_file?file_path=gwatch-0.0.1.tar.gz"
-  sha256 "bd7c3bfa9e36975ba3024550b1a17112f0a43105b1b175eaa2b8223696882123"
+  url "https://github.com/robertely/gwatch/releases/download/0.0.2/gwatch-0.0.2.tar.gz"
+  sha256 "c157202a3dd1d1f1cf176ccdc5d80a90c75fc60fc3846ff545c8311693ef40cc"
 
   bottle :unneeded
 
